47c993df34835e229163df729e751be2361bc4e9
andreyklaveren <you@example.comandreyklaveren@gmail.com>
2024-03-09 01:25:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './styles.css'
+import  api  from "./services/api";
 
-function App() {
+export default function App() {
+  
+  const [input, setInput] = useState("")
+  const [cep, setCep] = useState({})
+
+  async function handleSearch() {
+    if(input  === ""){
+      alert('Digite um CEP')
+      return;
+    }
+    if (!/^\d{8}$/.test(input)) {
+      alert('Digite um CEP válido com 8 números sem espaços e traço');
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput("")
+
+    } catch {
+      alert("Erro");
+      setInput("")
+    }
+  }
+  
+  function cL() {
+    console.log(cep)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu CEP"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="white"/>
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && //Se o  tamanho do objeto for maior que zero ele vai mostrar os dados do <main> 
+      <main className="main">
+        <h2>CEP: {cep.cep}</h2>
+        <span>Rua: {cep.logradouro}</span>
+
+        {cep.complemento && // se  o complemento não tiver valor exibe apenas a rua e o bairro
+          <span>Complemento: {cep.complemento}</span>
+        }
+        <span>Bairro: {cep.bairro}</span>
+        <span>Cidade-Estado: {cep.localidade} - {cep.uf}</span>
+        {cL()}
+      </main>
+      }
     </div>
   );
 }
-
-export default App;
